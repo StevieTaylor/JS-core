32b732306119f771cfcfd0b4eda22b826dfe2c38
stevie <galei111@126.com>
2022-12-02 20:17:08 +0800

    feat:apply

--- src/chapter/base/apply.js
+++ src/chapter/base/apply.js
@@ -2,8 +2,14 @@
  * @Author: Stevie
  * @Date: 2022-12-01 21:30:17
  * @LastEditors: Stevie
- * @LastEditTime: 2022-12-02 11:11:54
+ * @LastEditTime: 2022-12-02 20:15:57
  * @Description: apply
+ */
+/**
+ * @apply的特点
+ * - 功能：函数调用改变this指向
+ * - 参数：单个数组
+ * - 返回结果：直接执行
  */
 let a = {
   score: 85,
@@ -13,7 +19,7 @@ function getScore(name, age) {
   console.log({ name, age, score: this.score })
 }
 
-console.log('原始 apply')
+console.log('------------------------原生 apply------------------------')
 // * case1: 无参数
 getScore.apply(a)
 // * case2: 一个参数
@@ -21,30 +27,37 @@ getScore.apply(a, ['cxk'])
 // * case3: 多个参数
 getScore.apply(a, ['cxk', 24])
 
+/**
+ * @description:
+ * @param {*} target 待执行目标对象
+ * @param {*} argsArray 由参数组成的数组，可能为空undefined
+ * @return {*}
+ */
 Function.prototype._apply = function (target, argsArray) {
   target = target || window
   let fn = Symbol()
   target[fn] = this
+  // - 判断是否存在参数列表，若存在，解构带参数执行；若不存在，直接执行
   let result = argsArray ? target[fn](...argsArray) : target[fn]()
   delete target[fn]
   return result
 }
 
-console.log('参数列表版 apply')
+console.log('------------------------参数列表版 apply------------------------')
 getScore._apply(a)
 getScore._apply(a, ['cxk'])
 getScore._apply(a, ['cxk', 24])
 
-Function.prototype.myApply = function (ctx) {
-  let context = ctx || window
+Function.prototype.myApply = function (target) {
+  target = target || window
   const fn = Symbol()
-  context[fn] = this
-  let result = arguments[1] ? context[fn](...arguments[1]) : context[fn]()
-  delete context[fn]
+  target[fn] = this
+  let result = arguments[1] ? target[fn](...arguments[1]) : target[fn]()
+  delete target[fn]
   return result
 }
 
-console.log('执行上下文版 apply')
+console.log('------------------------arguments获取参数版 apply------------------------')
 getScore.myApply(a)
 getScore.myApply(a, ['cxk'])
 getScore.myApply(a, ['cxk', 24])
