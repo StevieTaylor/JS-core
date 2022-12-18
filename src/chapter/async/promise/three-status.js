0f92592fc3549050639dd74a59c99628a0b737bb
stevie <galei111@126.com>
2022-12-18 17:32:13 +0800

    feat:promise 3 status

--- src/chapter/async/promise/three-status.js
+++ src/chapter/async/promise/three-status.js
@@ -2,15 +2,14 @@
  * @Author: Stevie
  * @Date: 2022-12-18 17:19:53
  * @LastEditors: Stevie
- * @LastEditTime: 2022-12-18 17:20:24
+ * @LastEditTime: 2022-12-18 17:31:31
  * @Description: Promise 的3种状态
  */
-/**
- * - 1.Promise 的3种状态：pending,fulfilled,rejected
- * -   1.1 pending: 待定，初始状态，
- * -   1.2 fulfilled: 操作成功
- * -   1.3 rejected: 操作失败
- */
+
+//  - 1.Promise 的3种状态：pending,fulfilled,rejected
+//  -   1.1 pending: 待定，初始状态
+//  -   1.2 fulfilled: 操作成功
+//  -   1.3 rejected: 操作失败
 console.log('------------------------1.Promise 3种状态------------------------')
 
 function threeStatus(status = '', flag) {
